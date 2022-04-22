509e3124159caaa6d584d15625aa57ae379644e9
EduardoRojasM <83146468+EduardoRojasM@users.noreply.github.com>
2022-04-22 12:14:11 -0500

    Add files via upload

---
+++ Laboratorio-01/Ejercicio_15.cpp
@@ -0,0 +1,19 @@
+/*15. Escribir un programa que genere la tabla de multiplicar de un número introducido por
+el teclado.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"De que numero deseas la tabla de multiplicar: ";
+    cin>>num;
+
+    for (int i=0;i<=12;i++){
+        int res=0;
+        res=num*i;
+        cout<<num<<" x "<<i<<" = "<<res<<endl;
+    }
+    return 0;
+}
