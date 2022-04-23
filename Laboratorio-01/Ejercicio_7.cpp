9f0c3c5ce85bd957ee3f9988f9bb0894531c98dc
EduardoRojasM <83146468+EduardoRojasM@users.noreply.github.com>
2022-04-23 12:38:48 -0500

    Add files via upload

---
+++ Laboratorio-01/Ejercicio_7.cpp
@@ -0,0 +1,57 @@
+/*7. Elabore un programa que solicite ingresar una hora del día (HH:MM en formato de
+cadena), solicite un tiempo en minutos a agregar, y retorne la hora de finalización (el
+formato de salida debe de estar en AM o PM según corresponda).*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int ingre=0,h=0,m=0,r=0,n=0;
+    float minsumados=0;
+    string A,P;
+    cout<<"Ingrese una hora del dia"<<endl;
+    cout<<"Ingrese la hora: ";
+    cin>>h;
+    cout<<"Ingrese los minutos: ";
+    cin>>m;
+    cout<<"Cuanto minutos desea agregar: ";
+    cin>>ingre;
+
+    minsumados = m+ingre;
+
+    if (minsumados==60){
+        h = h + 1;
+    }
+    else if (minsumados<60){
+        m = m + ingre;
+    }
+    else{
+        r = minsumados/60;
+        h = h + r;
+        m = minsumados - (60*r);
+    }    
+
+    //Ver si es AM O PM
+
+    int res = h/12;
+
+    if (res%2==0){
+        A = "AM";
+    }
+    else if (res%2==1){
+        A = "PM";
+    }
+
+    //Calcular si son mas de 12 horas para reiniciar
+
+    if(h>12){
+    n = h/12;
+    h = h - (12*n);
+    }
+
+    cout<<h<<":"<<m<<" "<<A;
+
+    return 0;
+}
